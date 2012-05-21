3758aaf35c9d8c246b1c6f9a4df5928eedc59a2f
Michael Mulich <michael.mulich@gmail.com>
2012-05-21 16:39:28 -0400

    New tools interface module.
    I just need a place to put CMF tool interfaces and doing one interface
    per module seems stupid. So why not add another module named tools.

---
+++ Products/CMFPlone/interfaces/tools.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from properties import IPropertiesTool
+from basetool import IPloneBaseTool
+from basetool import IPloneTool
+from basetool import IPloneCatalogTool
+from controlpanel import IControlPanel
+from interface import IInterfaceTool
+from migration import IMigrationTool
+from factory import IFactoryTool
+from translationservice import ITranslationServiceTool
